b014d126213cbe5f0519babea9d24f67acc3b6e4
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2025-02-19 15:58:00 -0300

    add mediatr, carter and marten DI

---
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using BuildingBlocks.CQRS;
+global using Carter;
+global using Mapster;
+global using Marten;
+global using MediatR;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+});
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
