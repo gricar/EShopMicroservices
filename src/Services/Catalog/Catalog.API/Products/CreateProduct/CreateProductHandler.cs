f68eca1f230417bc4e038012ce99434b3777e749
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2025-02-19 15:58:30 -0300

    feat: add create product

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //Save to Database
+        //IDocumentSession is an abstraction of database. We dont need to create additional layer (repository pattern...)
+        session.Store(product);
+
+        await session.SaveChangesAsync(cancellationToken);
+
+
+        return new CreateProductResult(product.Id);
+    }
+}
