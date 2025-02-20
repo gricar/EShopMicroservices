6c3d689ec5a953bd7311b2b7132aa517b2386dd0
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2025-02-19 21:41:03 -0300

    feat: add validation behavior into pipeline

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -1,5 +1,8 @@
-﻿global using BuildingBlocks.CQRS;
+﻿global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions.Handler;
 global using Carter;
+global using FluentValidation;
 global using Mapster;
 global using Marten;
 global using MediatR;
