86a15adbda24c0c9589a6cc7e05551a7703d6b14
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2025-02-21 17:09:35 -0300

    global usings

--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -1,3 +1,6 @@
 ﻿global using BuildingBlocks.CQRS;
 global using FluentValidation;
+global using Ordering.Application.Data;
 global using Ordering.Application.DTOs;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
