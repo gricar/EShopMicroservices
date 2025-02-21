a14a6b4a7cb5fd3f5ba9edeee5a7554db2b2e2c1
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2025-02-21 17:13:40 -0300

    feat: update order usecase

--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -2,5 +2,6 @@
 global using FluentValidation;
 global using Ordering.Application.Data;
 global using Ordering.Application.DTOs;
+global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
