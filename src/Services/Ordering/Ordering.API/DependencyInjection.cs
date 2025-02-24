904a7b4cd52079102e854d93bad087814d21ecd6
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2025-02-24 20:59:36 -0300

    feat: ordering api - add database health check

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API;
 
@@ -10,6 +12,9 @@ public static class DependencyInjection
 
         services.AddExceptionHandler<CustomExceptionHandler>();
 
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
+
         return services;
     }
 
@@ -18,6 +23,12 @@ public static class DependencyInjection
         app.MapCarter();
 
         app.UseExceptionHandler(options => { });
+
+        app.UseHealthChecks("/health",
+           new HealthCheckOptions
+           {
+               ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+           });
 
         return app;
     }
